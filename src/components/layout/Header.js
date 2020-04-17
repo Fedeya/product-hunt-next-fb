d4eb3a41b8ffcb589f904da0d7719660e8fd86d4
Fedeya <elfedeminaya@gmail.com>
2020-04-17 16:30:51 -0300

    [Feat] added conditional show user

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,26 +1,83 @@
 import Link from 'next/link';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
-import Search from '../ui/Search';
 import Navigation from './Navigation';
 
+import Search from '../ui/Search';
+import Button from '../ui/Button';
+
+const Nav = styled.nav`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.p`
+  color: var(--orange);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: 'Roboto Slab', serif;
+  margin-right: 2rem;
+  cursor: pointer;
+`
 
 export default function Header() {
+
+  const user = false;
+
   return (
-    <header>
-      <nav>
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gray3);
+        padding: 1rem 0;
+      `}
+    >
+      <Nav>
         <div>
-          <p>P</p>
+          <Link href="/">
+            <Logo>P</Logo>
+          </Link>
           <Search />
           <Navigation />
         </div>
 
-        <div>
-          <p>Hola: Federico</p>
-          <button type="button">Log Out</button>
-          <Link href="/">Login</Link>
-          <Link href="/">Create Account</Link>
+        <div
+          css={css`
+              display: flex;
+              align-items: center;
+          `}
+        >
+          {
+            user ? (
+              <>
+                <p
+                  css={css`
+                    margin-right: 2rem;
+                  `}
+                >
+                  Hola: Federico
+                </p>
+                <Button type="button" bgColor>Log Out</Button>
+              </>
+            ) : (
+              <>
+                <Link href="/">
+                  <Button bgColor>Login</Button>
+                </Link>
+                <Link href="/">
+                  <Button>Create Account</Button>
+                </Link>
+              </>
+            )
+          }
         </div>
-      </nav>
+      </Nav>
     </header>
   );
 }
