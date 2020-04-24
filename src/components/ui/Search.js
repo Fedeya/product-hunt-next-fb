7fad25931694a259ad2de7f882690c1508cc24b9
Fedeya <elfedeminaya@gmail.com>
2020-04-24 19:38:22 -0300

    [Feat] added styled for search component

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,9 +1,43 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const InputText = styled.input`
+  border: 1px solid var(--gray3);
+  padding: 1rem;
+  min-width: 300px;
+`;
+
+const SubmitButton = styled.button`
+  height: 3rem;
+  width: 3rem;
+  display: block;
+  background-size: 4rem;
+  background-image: url('/static/img/search.png');
+  background-repeat: no-repeat;
+  position: absolute;
+  right: 1rem;
+  top: 1px;
+  background-color: white;
+  border: none;
+  text-indent: -99999px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default function Search() {
   return (
-    <form>
-      <input type="text"/>
-      <button type="submit">Search</button>
+    <form
+      css={css`
+        position: relative;
+      `}
+    >
+      <InputText 
+        type="text"
+        placeholder="Search Product"
+      />
+      <SubmitButton type="submit">Search</SubmitButton>
     </form>
   );
 }
