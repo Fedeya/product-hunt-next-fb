819c011f235624ea49c6882c30365369a408358d
Fedeya <elfedeminaya@gmail.com>
2020-04-24 19:38:45 -0300

    [Feat] added styled and href for pages

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,23 +1,40 @@
 import Link from 'next/link';
+import styled from '@emotion/styled';
+
+const NavList = styled.ul`
+  padding-left: 2rem;
+  display: flex;
+
+  li > a {
+    font-size: 1.8rem;
+    margin-left: 2rem;
+    color: var(--gray2);
+    font-family: 'PT Sans', sans-serif;
+
+    &:last-of-type {
+      margin-block: 0;
+    }
+  }
+`;
 
 export default function Navigation() {
   return (
-    <ul>
+    <NavList>
       <li>
         <Link href="/">
           <a>Home</a>
         </Link>
       </li>
       <li>
-        <Link href="/">
+        <Link href="/populars">
           <a>Popular</a>
         </Link>
       </li>
       <li>
-        <Link href="/">
+        <Link href="/new-product">
           <a>New Product</a>
         </Link>
       </li>
-    </ul>
+    </NavList>
   );
 }
