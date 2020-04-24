5648cb5b8d195db032662fd5c37fdcaa98dc91e7
Fedeya <elfedeminaya@gmail.com>
2020-04-24 20:37:17 -0300

    [Feat] added hook, ui components, and validation

--- src/pages/create-account.js
+++ src/pages/create-account.js
@@ -1,39 +1,86 @@
+import { css } from '@emotion/core';
+
 import Layout from '../components/layout/Layout';
+import { Form, Field, SubmitButton, Error } from '../components/ui/Form';
+
+import useValidation from '../hooks/useValidation';
+import validateCreateAccount from '../validation/validateCreateAccount';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: ''
+};
 
 export default function CreateAccount() {
+
+  const createAccount = () => {
+    console.log('creating account');
+  }
+
+  const { values, errors, handleSubmit, handleChange, handleBlur } = useValidation(initialState, validateCreateAccount, createAccount);
+
+  const { name, email, password } = values;
+
   return (
     <Layout>
-      <h1>Create Account</h1>
-      <form>
-        <div>
+      <h1
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+        `}
+      >
+        Create Account
+      </h1>
+      <Form onSubmit={handleSubmit} noValidate>
+        <Field>
           <label htmlFor="name">Name</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             id="name"
+            name="name"
             placeholder="Your name"
+            value={name}
+            onChange={handleChange}
+            onBlur={handleBlur}
           />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input 
-            type="email" 
-            id="email"
-            placeholder="Your Email"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input 
-            type="password" 
-            id="password"
-            placeholder="********"
-          />
-        </div>
+        </Field>
 
-        <button type="submit">
+        {errors.name && <Error>{errors.name}</Error>}
+
+        <Field>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your Email"
+            value={email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </Field>
+        
+        {errors.email && <Error>{errors.email}</Error>}
+
+        <Field>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="********"
+            value={password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </Field>
+        {errors.password && <Error>{errors.password}</Error>}
+
+        <SubmitButton type="submit">
           Create Account
-        </button>
-      </form>
+        </SubmitButton>
+      </Form>
     </Layout>
   );
 }
